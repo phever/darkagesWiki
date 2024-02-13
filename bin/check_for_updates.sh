c2e0e2d086165ee40118c7ea809ada012480223c
phever <me@phever.dev>
2024-02-12 23:14:06 -0700

    try to fix logo

--- bin/check_for_updates.sh
+++ bin/check_for_updates.sh
@@ -5,6 +5,7 @@ cd $SCRIPT_DIR/..
 
 if [[ $(git status --porcelain) ]]; then
   git pull
+  venv/bin/python manage.py migrate
   systemctl restart gunicorn.service
   systemctl restart nginx.service
 fi
