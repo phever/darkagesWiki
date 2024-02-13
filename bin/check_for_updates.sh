5b81857162f32e01dd5ae5bca6489f1b414acb5f
phever <me@phever.dev>
2024-02-13 09:03:58 -0700

    fix refs

--- bin/check_for_updates.sh
+++ bin/check_for_updates.sh
@@ -10,7 +10,7 @@ same_commit(){
 # get the changes if any, but don't merge them yet
 git fetch origin
 # See if there are any changes
-if same_commit mainline origin/mainline
+if same_commit refs/heads/master refs/remotes/origin/master
 then
    echo "Everything up to date"
    exit 0
