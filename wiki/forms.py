21a783ca70034bd0e6cee3ce07ddfc674f470526
phever <me@phever.dev>
2024-02-25 03:07:17 -0700

    updates

--- wiki/forms.py
+++ wiki/forms.py
@@ -1,3 +1,4 @@
+import django.forms
 from django.forms import ModelForm
 from wiki.models import Weapon, Item
 
@@ -5,10 +6,12 @@ from wiki.models import Weapon, Item
 class NewWeaponForm(ModelForm):
     class Meta(object):
         model = Weapon
+        fields = django.forms.ALL_FIELDS
 
 
 class NewItemForm(ModelForm):
     class Meta(object):
         model = Item
+        fields = django.forms.ALL_FIELDS
 
 
